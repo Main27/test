1974f4e336883f6fcec1cb56cdd1bae42d83cd5f
Marco Inderbitzin <Marco Inderbitzin@DESKTOP-QDR8CVM>
2019-09-22 18:01:42 +0200

    Ergänzung

--- src/Beer.java
+++ src/Beer.java
@@ -4,6 +4,7 @@ public class Beer {
 	public static void main(String[] args) {
 		System.out.println("Bier ist gut");
 		System.out.println("Oh ja");
+		System.out.println("Lang lebe MVC!");
 		
 		
 	}
